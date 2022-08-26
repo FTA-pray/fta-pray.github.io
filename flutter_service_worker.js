17ea0b496dba019537908a122c1e8d998b2eaf58
FTA-pray <111386948+FTA-pray@users.noreply.github.com>
2022-08-26 14:34:04 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "assets/AssetManifest.json": "7175e4beaac090a63ce5df80372ebc74",
+"assets/assets/images/alfajr.png": "d3dddb52fb261dbfd72b6d8557c2aab1",
+"assets/assets/images/ewewe.png": "a0f26387cf81026f2b9ee7fc5fdce102",
+"assets/assets/images/moon.png": "c3ff629c7701cd73972a3b7e62900207",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "ea5cc57e18b0426661de175ed609aedb",
+"assets/NOTICES": "4f4824b81f5d4ccc711c3212d42097e9",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
@@ -18,9 +21,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4aae7449e4d84212b686fd0f66b238d4",
-"/": "4aae7449e4d84212b686fd0f66b238d4",
-"main.dart.js": "27f7a4c02586787ff693037e21e44a69",
+"index.html": "c526730e2552d078b72eba29310c5d38",
+"/": "c526730e2552d078b72eba29310c5d38",
+"main.dart.js": "92342865407731006eede6b2d740b546",
 "manifest.json": "6145061897ef8860c4318e52ecdbf7be",
 "version.json": "79aafc39acc6545caead17350b10f990"
 };
